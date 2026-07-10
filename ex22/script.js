a74a01cda4b27391138bff57ecb353b46142fbdf
DevThiagoCBM <thiago999011@gmail.com>
2026-07-10 19:55:59 -0300

    Update script.js

--- ex22/script.js
+++ ex22/script.js
@@ -1,5 +1,8 @@
 //Faça uma função matemática que calcula a média de notas. Use a palavra-chave return para enviar o resultado de volta e guarde-o em uma variável fora da função.
 function calcularMedia() {
     let notas = [8, 9, 10, 10, 9, 9, 10];
-    
+    for (let nota of notas) {
+        let soma = 0;
+        soma += nota
+    }
 };
